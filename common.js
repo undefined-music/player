ea9f9a58dc08e82a00b4a72b83df4ced4a55d551
unedfined-music <fakeassmusician@gmail.com>
2019-07-06 12:47:56 +0900

    Fix jquery load error

--- common.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-    if(!isChrome){
-        $('#iframeAudio').remove();
-    }
-    else{
-        $('#audioAudio').remove();
-    }
-}
